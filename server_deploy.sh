217d1806be2b6f0681539183f527b18f9e2a8485
jksa <jksa.work.1@gmail.com>
2022-03-06 09:11:55 +0200

    edit commit

--- server_deploy.sh
+++ server_deploy.sh
@@ -7,9 +7,9 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down) || true
     # Update codebase
-    git pull origin master
+    git pull origin main
 # Exit maintenance mode
 php artisan up
 
